6b840ee8b1ec636307340e0321ff6d96bace83a9
mzheldin <mzheldin@yandex.ru>
2018-10-15 22:09:32 +0300

    Класс для тестирования

---
+++ hw7/Calc.java
@@ -0,0 +1,15 @@
+package hw7;
+
+public class Calc {
+
+    int a = 1;
+    int b = 2;
+
+    public int sum(int c){
+        return a + b + c;
+    }
+    public boolean biggerThanZero(int d){
+        return (d > 0);
+    }
+
+}
